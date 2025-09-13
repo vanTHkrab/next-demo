830ea8b929ad283bbb9667af4ef4f5db0745d220
vanTHkrab <vanth2548@gmail.com>
2025-09-13 09:43:19 +0700

    Add detail

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -3,7 +3,12 @@ const ProfilePage = () => {
         <div className="p-4 bg-gray-100 text-center text-lg font-semibold">
             <h1 className="text-2xl mb-4">Welcome to Your Profile</h1>
             <p className="text-gray-700">This is your personal space where you can view and manage your account details.</p>
-            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Edit Profile</button>
+            {/*name surname std*/}
+            <div className="mt-6 p-4 bg-white rounded shadow-md inline-block text-left">
+                <p><span className="font-bold">Name:</span> Audsadawut (อัษฎาวุฒิ)</p>
+                <p><span className="font-bold">Surname:</span> Nakthungtao (นาคทุ่งเตา)</p>
+                <p><span className="font-bold">Student ID:</span> 66133505</p>
+            </div>
         </div>
     );
 }
